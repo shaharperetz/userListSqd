fb94e988f7c376b2fb84ec4785cb411ff8b892a4
shahar <shaharp2@gmail.com>
2020-11-26 13:51:11 +0200

    starting functionallity in products

--- src/components/productPreview.vue
+++ src/components/productPreview.vue
@@ -8,20 +8,74 @@
       />
       <p class="avatar-name">{{ product.author.name }}</p>
     </div>
+    <div class="img-desc">
+      <img
+        class="product-img"
+        :src="require(`@/assets/pImgs/${product.name}.jpg`)"
+        alt=""
+      />
+      <p class="inside-img-desc">{{ product.name }}</p>
+    </div>
+    <div class="product-actions">
+      <div class="price">
+        <p>{{ this.priceSign + product.price }}</p>
+      </div>
+      <div class="comments-likes">
+        <img
+          class="mini-icon"
+          :src="require('@/assets/icons/comment.png')"
+          alt=""
+        />
+        <img
+          v-if="heartSign"
+          class="mini-icon"
+          :src="require(`@/assets/icons/${heartSign}.png`)"
+          alt=""
+          ref="heart"
+          @click.stop="onLikeClicked(product.likes)"
+        />
+      </div>
+    </div>
   </section>
 </template>
 
 <script>
 export default {
-  props: ["product"],
+  props: {
+    product: Object,
+  },
+  data() {
+    return {
+      priceSign: "€",
+      heartSign: null,
+    };
+  },
+  mounted() {
+    this.checkHeartStatus();
+  },
+  methods: {
+    checkHeartStatus() {
+      console.log(this.product.likes.isPressed);
+      this.heartSign = this.product.likes.isPressed ? "redHeart" : "heart";
+      if (this.product.likes.isDissable) {
+        this.heartSign = "blackHeart";
+        console.log(this.$refs.heart);
+      }
+    },
+    onLikeClicked(likes) {
+      console.log(likes);
+    },
+  },
 };
 </script>
 
 <style lang="css">
 .product-prev {
   display: flex;
-  height: 200px;
-  width: 150px;
+  flex-direction: column;
+  /* height: 200px; */
+  margin-bottom: 20px;
+  width: 250px;
 }
 
 .product-headline {
@@ -33,8 +87,37 @@ export default {
 
 .avatar-img {
   margin-right: 1vw;
-  height: 30px;
   width: 30px;
   border-radius: 50%;
+}
+
+.product-img {
+  width: 100%;
+}
+
+.img-desc {
+  position: relative;
+}
+
+.inside-img-desc {
+  padding: 5px;
+  position: absolute;
+  bottom: 0;
+  line-height: 0;
+  color: white;
+  font-size: 1.2em;
+}
+
+.product-actions {
+  padding: 5px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.mini-icon {
+  margin-left: 5px;
+  width: 20px;
+  cursor: pointer;
 }
 </style>
--- src/modules/ProductsStore.js
+++ src/modules/ProductsStore.js
@@ -3,33 +3,48 @@ export default {
     state: {
         products: [
             {
-                name: 'Mountin fox notebook',
+                name: 'Mountain fox notebook',
+                _id: 01,
                 author: {
                     name: 'Gabriel',
                     id: 'gabriel'
                 },
                 price: 12.90,
-                likes: 0,
+                likes: {
+                    counter: 0,
+                    isPressed: true,
+                    isDissable: false
+                },
                 comments: [],
             },
             {
                 name: 'Brown bear cushion',
+                _id: 02,
                 author: {
                     name: 'Gabriel',
                     id: 'gabriel'
                 },
                 price: 18.90,
-                likes: 0,
+                likes: {
+                    counter: 0,
+                    isPressed: false,
+                    isDissable: false
+                },
                 comments: [],
             },
             {
                 name: 'Hummingbird cushion',
+                _id: 03,
                 author: {
                     name: 'Elysa',
                     id: 'elysa'
                 },
                 price: 18.90,
-                likes: 0,
+                likes: {
+                    counter: 0,
+                    isPressed: false,
+                    isDissable: true
+                },
                 comments: [],
             },
         ]
